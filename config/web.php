51e4f1ecd9b3cbb1d62187cf9e90b934a1772394
Konstantin Molchanov <kostya.molchanov@gmail.com>
2021-02-04 15:26:31 +0600

    First release

--- config/web.php
+++ config/web.php
@@ -38,7 +38,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'hll4RRZlrVRJ2FsRHsWdOLbXpTL3PJiP',
+            'cookieValidationKey' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
